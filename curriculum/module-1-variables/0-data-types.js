934f80c513c1f5c7162d807fa5431e8127720bc0
rafaelfeng <108007057+rafaelfeng@users.noreply.github.com>
2026-09-10 12:38:58 +0800

    Create 0-data-types.js

---
+++ curriculum/module-1-variables/0-data-types.js
@@ -0,0 +1,7 @@
+/* curriculum/module-1-variables/0-data-types.js */
+export default {
+  title: "Data Types",
+  theory: "<h2>Core Data Types</h2><p>Common types are <code>int</code>, <code>float</code>, and <code>char</code>.</p>",
+  starter: "float pi = 3.14;",
+  validate(src) { return true; }
+};
