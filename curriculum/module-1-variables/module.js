bff8fc7c58313b7a8c528288fd6a675923874218
rafaelfeng <108007057+rafaelfeng@users.noreply.github.com>
2026-09-10 12:39:13 +0800

    Create module.js

---
+++ curriculum/module-1-variables/module.js
@@ -0,0 +1,17 @@
+/* curriculum/module-1-variables/module.js */
+import lesson0 from './0-data-types.js';
+
+export default {
+  title: "Variables & Types",
+  level: "beginner",
+  time: "~1 hr",
+  desc: "Store, read and combine data using C's core built-in types.",
+  outline: [
+    "Declaring and initialising variables",
+    "<code>int</code>, <code>char</code>, <code>float</code> and <code>double</code>",
+    "Reading input with <code>scanf()</code>",
+    "Arithmetic and assignment operators",
+    "Type conversion and casting basics"
+  ],
+  sub_lessons: [lesson0]
+};
