ea2050c8057d43cf9d7a941cf37e5047d420218f
rafaelfeng <108007057+rafaelfeng@users.noreply.github.com>
2026-09-12 09:59:56 +0800

    Create module.js

---
+++ curriculum/module-4-structs/module.js
@@ -0,0 +1,21 @@
+/* curriculum/module-4-structs/module.js */
+import l0 from './0-defining-struct.js';
+import l1 from './1-arrays-of-structs.js';
+import l2 from './2-typedef.js';
+import l3 from './3-pointers-to-structs.js';
+import l4 from './4-linked-list.js';
+
+export default {
+  title: "Structs & Data Structures",
+  level: "advanced",
+  time: "~2 hrs",
+  desc: "Bundle related data together and build your first real data structures.",
+  outline: [
+    "Defining and using <code>struct</code>",
+    "Arrays of structs",
+    "<code>typedef</code> for cleaner code",
+    "Pointers to structs and the <code>-&gt;</code> operator",
+    "Your first linked list"
+  ],
+  sub_lessons: [l0, l1, l2, l3, l4]
+};
